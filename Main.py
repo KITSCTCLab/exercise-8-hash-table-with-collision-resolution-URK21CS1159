24c6ac7fe0eac240218d95cd863fb6b1bdbe840b
URK21CS1159 <110010698+URK21CS1159@users.noreply.github.com>
2022-09-20 11:34:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import re
 
-def display_hash(hashtable) -> None:
+def display_hash(Hashtable) -> None:
 	# Write your code here
-    for i in range(len(hashTable)):
+    for i in range(len(HashTable)):
         print(i, end = " ")
           
-        for j in hashTable[i]:
+        for j in HashTable[i]:
             print("-->", end = " ")
             print(j, end = " ")
               
@@ -14,23 +14,23 @@ def display_hash(hashtable) -> None:
 def Hashing(keyvalue) -> int:
 	return keyvalue % len(HashTable)
 
-def insert(hashtable, keyvalue, value) -> None:
+def insert(Hashtable, keyvalue, value) -> None:
 	# Write your code here
 	hash_key = Hashing(keyvalue)
-    hashtable[hash_key].append(value)
+    Hashtable[hash_key].append(value)
   
 
 
 # Do not edit the following code
 hash_table_size = int(input())
 # Create Hashtable as a list of list.
-hashTable = [[] for _ in range(hash_table_size)]
+HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
   item = item[1:]
   data = item.split(', ')
   if len(data) > 1:
-    insert(hashTable, int(data[0]), data[1])
+    insert(HashTable, int(data[0]), data[1])
 
-display_hash (hashTable)
+display_hash (HashTable)
