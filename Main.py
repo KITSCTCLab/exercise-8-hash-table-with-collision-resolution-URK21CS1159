ac6741ef7588d562dcf7ae7b73cd94543af84a0c
URK21CS1159 <110010698+URK21CS1159@users.noreply.github.com>
2022-09-20 11:29:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,13 +24,13 @@ def insert(hashtable, keyvalue, value) -> None:
 # Do not edit the following code
 hash_table_size = int(input())
 # Create Hashtable as a list of list.
-HashTable = [[] for _ in range(hash_table_size)]
+hashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
   item = item[1:]
   data = item.split(', ')
   if len(data) > 1:
-    insert(HashTable, int(data[0]), data[1])
+    insert(hashTable, int(data[0]), data[1])
 
-display_hash (HashTable)
+display_hash (hashTable)
